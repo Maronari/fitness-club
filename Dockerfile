327d756d36878d1aa5d8b51f29d5463a383b45dd
Maronari <ivan.corsunov@gmail.com>
2024-03-17 21:43:58 +0300

    update

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM eclipse-temurin:20-jdk-alpine  
-ARG JAR_FILE=target/fitness_club-0.5.jar 
+ARG JAR_FILE=target/fitness_club-0.8.jar 
 WORKDIR /opt/app  
 COPY ${JAR_FILE} fitness_club.jar 
 ENTRYPOINT ["java","-jar","fitness_club.jar"]
--- start.sh
+++ start.sh
@@ -8,9 +8,11 @@ if [ $rc -ne 0 ] ; then
     echo Could not perform mvn clean install, exit code [$rc]; exit $rc
 fi
 
+docker volume rm fitness_club_db-data
+
 # Ensure, that docker-compose doesn't exist
 docker-compose down
 
 # Start new deployment
-docker-compose up --build -d
+docker-compose up --build
 
